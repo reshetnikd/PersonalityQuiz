d240544934b1f95c42c63371659cbb2958669929
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-14 20:12:30 +0300

    Models and outlets

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    var responses: [Answer]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 
