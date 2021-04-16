5dd8befa4c16f4e204029d0155f1d48c81604286
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-16 12:22:30 +0300

    Add labels to visualize the app life cycle

--- PersonalityQuiz/IntroductionViewController.swift
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class IntroductionViewController: UIViewController {
+    var appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+    var willConnectToCount = 0
+    var didBecomeActiveCount = 0
+    var willResignActiveCount = 0
+    var willEnterForegroundCount = 0
+    var didEnterBackgroundCount = 0
+    
+    @IBOutlet weak var didFinishLaunchingLabel: UILabel!
+    @IBOutlet weak var configurationForConnectingLabel: UILabel!
+    @IBOutlet weak var willConnectToLabel: UILabel!
+    @IBOutlet weak var didBecomeActiveLabel: UILabel!
+    @IBOutlet weak var willResignActiveLabel: UILabel!
+    @IBOutlet weak var willEnterForegroundLabel: UILabel!
+    @IBOutlet weak var didEnterBackgroundLabel: UILabel!
+    
     @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
         
     }
@@ -15,6 +30,16 @@ class IntroductionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateView() {
+        didFinishLaunchingLabel.text = "-application(_:didFinishLaunchingWithOptions:) \(appDelegate.launchCount) time(s)"
+        configurationForConnectingLabel.text = "-application(_:configurationForConnecting:options:) \(appDelegate.configurationForConnectingCount) time(s)"
+        willConnectToLabel.text = "-scene(_:willConnectTo:options:) \(willConnectToCount) time(s)"
+        didBecomeActiveLabel.text = "-sceneDidBecomeActive(_:) \(didBecomeActiveCount) time(s)"
+        willResignActiveLabel.text = "-sceneWillResignActive(_:) \(willResignActiveCount) time(s)"
+        willEnterForegroundLabel.text = "-sceneWillEnterForeground(_:) \(willEnterForegroundCount) time(s)"
+        didEnterBackgroundLabel.text = "-sceneDidEnterBackground(_:) \(didEnterBackgroundCount) time(s)"
     }
 
 
--- PersonalityQuiz/SceneDelegate.swift
+++ PersonalityQuiz/SceneDelegate.swift
@@ -10,13 +10,16 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var viewController: IntroductionViewController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        print("Scene will connect to session.")
+        viewController = window?.rootViewController as? IntroductionViewController
+        viewController?.willConnectToCount += 1
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -24,27 +27,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        print("Scene did disconnect.")
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print("Scene did become active.")
+        viewController?.didBecomeActiveCount += 1
+        viewController?.updateView()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print("Scene will resign active.")
+        viewController?.willResignActiveCount += 1
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        print("Scene will enter foreground.")
+        viewController?.willEnterForegroundCount += 1
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        print("Scene did enter background.")
+        viewController?.didEnterBackgroundCount += 1
     }
 
 
